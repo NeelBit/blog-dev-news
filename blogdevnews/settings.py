f9964c4a9683ef85a6d1bf9539323251d4203e2a
NeelBit <nelbertran@gmail.com>
2025-07-07 20:01:19 -0300

    Correction sec. and environment variable

--- blogdevnews/settings.py
+++ blogdevnews/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+# importación necesaria para la variable de entorno para la configuración de Django
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=+#zei!c(28$&o$kuoxx8*rdcssome(-$c)kj3cobup-7=2uoj'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
